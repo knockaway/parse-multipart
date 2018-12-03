11587a27740552e4561b12f4c8b55133eeb781de
Bigi Lui <bigi@knock.com>
2018-12-03 15:30:05 -0800

    Dont need DemoData in package

--- multipart.js
+++ multipart.js
@@ -142,24 +142,3 @@ exports.getBoundary = function(header) {
     }
   return "";
 };
-
-exports.DemoData = function() {
-  body = "trash1\r\n";
-  body += "------WebKitFormBoundaryvef1fLxmoUdYZWXp\r\n";
-  body +=
-    'Content-Disposition: form-data; name="uploads[]"; filename="A.txt"\r\n';
-  (body += "Content-Type: text/plain\r\n"), (body += "\r\n\r\n");
-  body += "@11X";
-  body += "111Y\r\n";
-  body += "111Z\rCCCC\nCCCC\r\nCCCCC@\r\n\r\n";
-  body += "------WebKitFormBoundaryvef1fLxmoUdYZWXp\r\n";
-  body +=
-    'Content-Disposition: form-data; name="uploads[]"; filename="B.txt"\r\n';
-  (body += "Content-Type: text/plain\r\n"), (body += "\r\n\r\n");
-  body += "@22X";
-  body += "222Y\r\n";
-  body += "222Z\r222W\n2220\r\n666@\r\n";
-  body += "------WebKitFormBoundaryvef1fLxmoUdYZWXp--\r\n";
-  return new Buffer(body, "utf-8");
-  // returns a Buffered payload, so the it will be treated as a binary content.
-};
